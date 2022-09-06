390332a107406943e282153b0e82225578b12d18
M4RZ3R0 <30533649+M4RZ3R0@users.noreply.github.com>
2022-09-06 15:43:26 +0200

    Implemented reset, timers (partially), and fetch

--- chip8_core/src/lib.rs
+++ chip8_core/src/lib.rs
@@ -10,6 +10,26 @@ const NUM_REGS: usize = 16;
 const STACK_SIZE: usize = 16;
 const NUM_KEYS: usize = 16;
 
+const FONTSET_SIZE: usize = 80;
+const FONTSET: [u8; FONTSET_SIZE] = [
+    0xF0, 0x90, 0x90, 0x90, 0xF0, // 0
+    0x20, 0x60, 0x20, 0x20, 0x70, // 1
+    0xF0, 0x10, 0xF0, 0x80, 0xF0, // 2
+    0xF0, 0x10, 0xF0, 0x10, 0xF0, // 3
+    0x90, 0x90, 0xF0, 0x10, 0x10, // 4
+    0xF0, 0x80, 0xF0, 0x10, 0xF0, // 5
+    0xF0, 0x80, 0xF0, 0x90, 0xF0, // 6
+    0xF0, 0x10, 0x20, 0x40, 0x40, // 7
+    0xF0, 0x90, 0xF0, 0x90, 0xF0, // 8
+    0xF0, 0x90, 0xF0, 0x10, 0xF0, // 9
+    0xF0, 0x90, 0xF0, 0x90, 0x90, // A
+    0xE0, 0x90, 0xE0, 0x90, 0xE0, // B
+    0xF0, 0x80, 0x80, 0x80, 0xF0, // C
+    0xE0, 0x90, 0x90, 0x90, 0xE0, // D
+    0xF0, 0x80, 0xF0, 0x80, 0xF0, // E
+    0xF0, 0x80, 0xF0, 0x80, 0x80, // F
+];
+
 pub struct Machine {
     pc: u16,
     ram: [u8; RAM_SIZE],
@@ -24,7 +44,7 @@ pub struct Machine {
 
 impl Machine {
     pub fn new() -> Self {
-        Self {
+        let mut new_machine = Self {
             pc: START_ADDR,
             ram: [0; RAM_SIZE],
             screen: [false; SCREEN_WIDTH * SCREEN_HEIGHT],
@@ -34,6 +54,64 @@ impl Machine {
             keys: [false; NUM_KEYS],
             dt: 0,
             st: 0,
+        };
+
+        new_machine.ram[..FONTSET_SIZE].copy_from_slice(&FONTSET);
+
+        new_machine
+    }
+
+    pub fn reset(&mut self) {
+        self.pc = START_ADDR;
+        self.ram = [0; RAM_SIZE];
+        self.ram[..FONTSET_SIZE].copy_from_slice(&FONTSET);
+        self.screen = [false; SCREEN_WIDTH * SCREEN_HEIGHT];
+        self.v_reg = [0; NUM_REGS];
+        self.i_reg = 0;
+        self.stack = VecDeque::with_capacity(STACK_SIZE);
+        self.keys = [false; NUM_KEYS];
+        self.dt = 0;
+        self.st = 0;
+    }
+
+    pub fn push(&mut self, val: u16) {
+        self.stack.push_back(val);
+    }
+
+    pub fn pop(&mut self) -> u16 {
+        if let Some(i) = self.stack.pop_back() {
+            i
+        } else {
+            println!("Error: stack is empty");
+            std::process::exit(5);
         }
+    }
+
+    pub fn tick_timers(&mut self) {
+        if self.dt > 0 {
+            self.dt -= 1;
+        }
+
+        if self.st > 0 {
+            if self.st == 1 {
+                // BEEP
+            }
+            self.st -= 1;
+        }
+    }
+
+    pub fn tick(&mut self) {
+        // Fetch
+        let op = self.fecth();
+        // Decode
+        // Execute
+    }
+
+    fn fecth(&mut self) -> u16 {
+        let higher_byte = self.ram[self.pc as usize] as u16;
+        let lower_byte = self.ram[(self.pc + 1) as usize] as u16;
+        self.pc += 2;
+
+        (higher_byte << 8) | lower_byte
     }
 }
