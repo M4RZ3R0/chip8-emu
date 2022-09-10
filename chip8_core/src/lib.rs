8807452552855947b66b1a988beb739f69d4c523
M4RZ3R0 <30533649+M4RZ3R0@users.noreply.github.com>
2022-09-10 13:34:02 +0200

    Finished CPU

--- chip8_core/src/lib.rs
+++ chip8_core/src/lib.rs
@@ -1,3 +1,4 @@
+use rand::random;
 use std::collections::VecDeque;
 
 pub const SCREEN_WIDTH: usize = 64;
@@ -40,6 +41,12 @@ pub struct Machine {
     keys: [bool; NUM_KEYS],
     dt: u8,
     st: u8,
+}
+
+impl Default for Machine {
+    fn default() -> Self {
+        Self::new()
+    }
 }
 
 impl Machine {
@@ -103,8 +110,8 @@ impl Machine {
     pub fn tick(&mut self) {
         // Fetch
         let op = self.fecth();
-        // Decode
-        // Execute
+        // Decode & execute
+        self.execute(op);
     }
 
     fn fecth(&mut self) -> u16 {
@@ -113,5 +120,150 @@ impl Machine {
         self.pc += 2;
 
         (higher_byte << 8) | lower_byte
+    }
+
+    fn execute(&mut self, op: u16) {
+        let byte1 = (op & 0xF000) >> 12;
+        let byte2 = (op & 0x0F00) >> 8;
+        let byte3 = (op & 0x00F0) >> 4;
+        let byte4 = (op & 0x000F) >> 2;
+
+        match (byte1, byte2, byte3, byte4) {
+            (0, 0, 0xE, 0) => self.screen = [false; SCREEN_WIDTH * SCREEN_HEIGHT],
+            (0, 0, 0xE, 0xE) => self.pc = self.pop(),
+            (1, _, _, _) => self.pc = op & 0xFFF,
+            (2, _, _, _) => {
+                self.push(self.pc);
+                self.pc = op & 0xFFF
+            }
+            (3, _, _, _) => {
+                if self.v_reg[byte2 as usize] == (op & 0xFF) as u8 {
+                    self.pc += 2
+                }
+            }
+            (5, _, _, 0) => {
+                if self.v_reg[byte2 as usize] == self.v_reg[byte3 as usize] {
+                    self.pc += 2
+                }
+            }
+            (6, _, _, _) => self.v_reg[byte2 as usize] = (op & 0xFF) as u8,
+            (7, _, _, _) => {
+                self.v_reg[byte2 as usize] =
+                    self.v_reg[byte2 as usize].wrapping_add((op & 0xFF) as u8)
+            }
+            (8, _, _, 0) => self.v_reg[byte2 as usize] = self.v_reg[byte3 as usize],
+            (8, _, _, 1) => self.v_reg[byte2 as usize] |= self.v_reg[byte3 as usize],
+            (8, _, _, 2) => self.v_reg[byte2 as usize] &= self.v_reg[byte3 as usize],
+            (8, _, _, 3) => self.v_reg[byte2 as usize] ^= self.v_reg[byte3 as usize],
+            (8, _, _, 4) => {
+                let carry;
+                (self.v_reg[byte2 as usize], carry) =
+                    self.v_reg[byte2 as usize].overflowing_add(self.v_reg[byte3 as usize]);
+
+                self.v_reg[0xF] = if carry { 1 } else { 0 };
+            }
+            (8, _, _, 5) => {
+                let borrow;
+                (self.v_reg[byte2 as usize], borrow) =
+                    self.v_reg[byte2 as usize].overflowing_sub(self.v_reg[byte3 as usize]);
+
+                self.v_reg[0xF] = if borrow { 1 } else { 0 };
+            }
+            (8, _, _, 6) => {
+                self.v_reg[0xF] = self.v_reg[byte2 as usize] & 1;
+                self.v_reg[byte2 as usize] >>= 1;
+            }
+            (8, _, _, 7) => {
+                let borrow;
+                (self.v_reg[byte2 as usize], borrow) =
+                    self.v_reg[byte3 as usize].overflowing_sub(self.v_reg[byte2 as usize]);
+
+                self.v_reg[0xF] = if borrow { 1 } else { 0 };
+            }
+            (8, _, _, 0xE) => {
+                self.v_reg[0xF] = (self.v_reg[byte2 as usize] >> 7) & 1;
+                self.v_reg[byte2 as usize] <<= 1;
+            }
+            (9, _, _, 0) => {
+                if self.v_reg[byte2 as usize] != self.v_reg[byte3 as usize] {
+                    self.pc += 2
+                }
+            }
+            (0xA, 0, 0, 0) => self.i_reg = op & 0xFFF,
+            (0xB, _, _, _) => self.pc = (self.v_reg[0] as u16) + (op & 0xFFF),
+            (0xC, _, _, _) => self.v_reg[byte2 as usize] = random::<u8>() & (op & 0xFF) as u8,
+            (0xD, _, _, _) => {
+                let x_start = self.v_reg[byte2 as usize] as u16;
+                let y_start = self.v_reg[byte3 as usize] as u16;
+
+                let mut flipped = false;
+
+                for j in 0..byte4 {
+                    let pixels = self.ram[(self.i_reg + j) as usize];
+
+                    for i in 0..8 {
+                        if pixels & (0b10000000 >> i) != 0 {
+                            let x = (x_start + i) as usize % SCREEN_WIDTH;
+                            let y = (y_start + j) as usize % SCREEN_HEIGHT;
+
+                            let index = y * SCREEN_WIDTH + x;
+                            flipped |= self.screen[index];
+                            self.screen[index] ^= true;
+                        }
+                    }
+                }
+
+                self.v_reg[0xF] = if flipped { 1 } else { 0 };
+            }
+            (0xE, _, 9, 0xE) => {
+                if self.keys[self.v_reg[byte2 as usize] as usize] {
+                    self.pc += 2
+                }
+            }
+            (0xE, _, 0xA, 1) => {
+                if !self.keys[self.v_reg[byte2 as usize] as usize] {
+                    self.pc += 2
+                }
+            }
+            (0xF, _, 0, 7) => self.v_reg[byte2 as usize] = self.dt,
+            (0xF, _, 0, 0xA) => {
+                let mut pressed = false;
+                for i in 0..self.keys.len() {
+                    if self.keys[i] {
+                        self.v_reg[byte2 as usize] = i as u8;
+                        pressed = true;
+                        break;
+                    }
+                }
+
+                if !pressed {
+                    self.pc -= 2;
+                }
+            }
+            (0xF, _, 1, 5) => self.dt = self.v_reg[byte2 as usize],
+            (0xF, _, 1, 8) => self.st = self.v_reg[byte2 as usize],
+            (0xF, _, 1, 0xE) => {
+                self.i_reg = self.i_reg.wrapping_add(self.v_reg[byte2 as usize] as u16)
+            }
+            (0xF, _, 2, 9) => self.i_reg = (self.v_reg[byte2 as usize] * 5) as u16,
+            (0xF, _, 3, 3) => {
+                let mut vx = self.v_reg[byte2 as usize];
+                for i in 0..3 {
+                    self.ram[(self.i_reg + (3 - i)) as usize] = vx % 10;
+                    vx /= 10;
+                }
+            }
+            (0xF, _, 5, 5) => {
+                for index in 0..=byte2 {
+                    self.ram[(self.i_reg + index) as usize] = self.v_reg[index as usize];
+                }
+            }
+            (0xF, _, 6, 5) => {
+                for index in 0..=byte2 {
+                    self.v_reg[index as usize] = self.ram[(self.i_reg + index) as usize];
+                }
+            }
+            (_, _, _, _) => (),
+        };
     }
 }
